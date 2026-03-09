72e35cc433eb9836e023ea4f3c91a3694975ce9b
Christian <christianda3@gmail.com>
2026-03-09 17:50:17 -0400

    Some things idk

--- program/garmin_core/src/classification/mod.rs
+++ program/garmin_core/src/classification/mod.rs
@@ -10,11 +10,11 @@ pub use terrain::Terrain;
 pub fn classify_intensity(activity: &Activity) -> Result<Intensity, ClassError> {
     let avg = activity
         .avg_hr
-        .ok_or(ClassError::MissingMetrics("avg_hr missing"))?;
+        .ok_or(ClassError::MissingMetrics("avg_hr missing".to_string()))?;
 
     let max = activity
         .max_hr
-        .ok_or(ClassError::MissingMetrics("max_hr missing"))?;
+        .ok_or(ClassError::MissingMetrics("max_hr missing".to_string()))?;
 
     let intensity_ratio = avg.0 as f64 / max.0 as f64;
 
--- program/garmin_core/src/classification/terrain.rs
+++ program/garmin_core/src/classification/terrain.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone)]
 pub enum Terrain {
     Flat,
     Rolling,
